95911bb5d16199a50048d0d5ce2f611ba3ff7c81
Alper Akinci <alper.akinci@adastra.one>
2018-09-14 17:09:46 +0200

    Add leaf files ✨

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,39 @@
 import Vapor
+import Leaf
+
+let dev = Developer(name: "Alper", title: "iOS Developer", company: "Adastra.one")
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
+
+    // example: http://localhost:8002/name
+
+    router.get("name") { req in
+        return dev.name
     }
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.get("title") { req in
+        return dev.title
+    }
+
+    router.get("json") { req in
+        return dev
+    }
+
+/// The function req.view() creates and returns a ViewRenderer.
+/// And since we configured LeafRenderer as our ViewRenderer we can use .leaf files 🍃!
+    router.get("welcome") { req -> Future<View> in
+        return try req.view().render("welcome")
+    }
+
+    router.get("aboutMe") { req -> Future<View> in
+        return try req.view().render("aboutMe", dev)
+    }
+
+}
+
+struct Developer: Content {
+    var name: String
+    var title: String
+    var company: String
 }
